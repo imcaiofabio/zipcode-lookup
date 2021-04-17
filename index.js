18d982a5fd315bd868380d8c4781f85d2659c3f9
imcaiofabio <caio.fabio91@gmail.com>
2021-04-17 00:34:54 -0300

    lookup function created

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-import soap from 'soap';
+var soap = require('soap');
+var url = "https://apps.correios.com.br/SigepMasterJPA/AtendeClienteService/AtendeCliente?wsdl";
+var args = {cep: '18071704'};
 
-
+soap.createClient(url, function(err, client){
+    client.consultaCEP(args, function(err, result) {
+        console.log(result);
+    });
+});
 
